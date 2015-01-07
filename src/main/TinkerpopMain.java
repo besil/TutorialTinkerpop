5198858072606e0de11d128c35ffc9f695aba779
besil <s.bernardinello@reply.it>
2015-01-07 13:33:13 +0100

    Full addVertex working

--- src/main/TinkerpopMain.java
+++ src/main/TinkerpopMain.java
@@ -1,9 +1,15 @@
 package main;
 
-import tutorial.Tutorial26;
+import tutorial.Tutorial30;
+
 
 public class TinkerpopMain {
 	public static void main(String[] args) {
-		new Tutorial26().execute();
+//		new Tutorial26().execute();
+		try {
+			new Tutorial30().execute();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
--- src/tutorial/Tutorial30.java
+++ src/tutorial/Tutorial30.java
@@ -1,5 +1,35 @@
 package tutorial;
 
-public class Tutorial30 {
+import java.io.File;
 
+import org.apache.commons.io.FileUtils;
+
+import com.tinkerpop.gremlin.neo4j.structure.Neo4jGraph;
+import com.tinkerpop.gremlin.structure.Graph;
+import com.tinkerpop.gremlin.structure.Vertex;
+
+public class Tutorial30 {
+	public void execute() throws Exception {
+		String dir = "data/neograph";
+		FileUtils.deleteDirectory(new File(dir));
+//		Graph g = TinkerFactory.createClassic();
+		Graph g = Neo4jGraph.open(dir);
+//		Graph g = TinkerGraph.open();
+//		int vc = 0, ec=0;
+		
+		Vertex marko = g.addVertex(		"name", "marko", "age", 29);
+		Vertex vadas = g.addVertex(		"name", "vadas", "age", 27);
+		Vertex lop = g.addVertex(		"name", "lop", "lang", "java");
+		Vertex josh = g.addVertex(		"name", "josh", "age", 32);
+		Vertex ripple = g.addVertex(	"name", "ripple", "lang", "java");
+		Vertex peter = g.addVertex(		"name", "peter", "age", 35);
+		marko.addEdge("knows", vadas, 	"weight", 0.5f);
+		marko.addEdge("knows", josh, 	"weight", 1.0f);
+		marko.addEdge("created", lop, 	"weight", 0.4f);
+		josh.addEdge("created", ripple, "weight", 1.0f);
+		josh.addEdge("created", lop, 	"weight", 0.4f);
+		peter.addEdge("created", lop, 	"weight", 0.2f);
+
+		g.close();
+	}
 }
