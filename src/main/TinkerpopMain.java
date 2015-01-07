d05520f789d51cc35a55f2a0a42593a92c1ce7de
besil <s.bernardinello@reply.it>
2015-01-07 13:02:51 +0100

    Refactoring

--- src/main/TinkerpopMain.java
+++ src/main/TinkerpopMain.java
@@ -1,24 +1,9 @@
 package main;
 
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.Map;
-
-import com.google.common.collect.ImmutableSortedMap;
-import com.tinkerpop.blueprints.Edge;
-import com.tinkerpop.blueprints.Graph;
-import com.tinkerpop.blueprints.Vertex;
-import com.tinkerpop.blueprints.impls.neo4j2.Neo4j2Graph;
-import com.tinkerpop.blueprints.impls.tg.TinkerGraphFactory;
-import com.tinkerpop.furnace.algorithms.vertexcentric.GraphComputer;
-import com.tinkerpop.furnace.algorithms.vertexcentric.VertexMemory;
-import com.tinkerpop.furnace.algorithms.vertexcentric.computers.SerialGraphComputer;
-import com.tinkerpop.furnace.algorithms.vertexcentric.programs.ranking.DegreeRankProgram;
-import com.tinkerpop.furnace.algorithms.vertexcentric.programs.ranking.PageRankProgram;
+import tutorial.Tutorial26;
 
 public class TinkerpopMain {
 	public static void main(String[] args) {
+		new Tutorial26().execute();
 	}
-	
-	
 }
